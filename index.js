8ad700abc2df3bc0710eb76e3cee2fe013c683fb
RumiYo <yokoyamarumi@gmail.com>
2023-06-13 00:51:43 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-/*
-function monday(){
-    console.log("Go for a five-mile run");
-    console.log("Pump iron");
-}
-function tuesday(){
-    console.log("Go for a five-mile run");
-    console.log("Swim 40 laps");
-}
-function wednesday(){
-    console.log("Go for a five-mile run");
-    console.log("Go for a five-mile run");
-}
-function thursday(){
-    console.log("Go for a five-mile run");
-    console.log("Pump iron");
-}
-function friday(){
-    console.log("Go for a five-mile run");
-    console.log("Swim 40 laps");
-}
-*/
 function receivesAFunction(callback){
     return callback();
 }
